358a9b2514db52f1ca23d7a17279292a8d02a0a0
sv6095 <shantanues234@gmail.com>
2025-09-30 15:58:28 +0530

    Fixed issues in backend

--- SIGAPP/Frontend/src/components/Footer.tsx
+++ SIGAPP/Frontend/src/components/Footer.tsx
@@ -37,7 +37,7 @@ const Footer = () => {
     setMessage("");
 
     try {
-      const res = await fetch("http://localhost:5000/subscribe", {
+      const res = await fetch("https://acm-sigapp-1.onrender.com/subscribe", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
